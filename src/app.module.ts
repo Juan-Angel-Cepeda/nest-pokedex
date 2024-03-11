5203fdb5ddae933f97976a13d3d1c6dd75b7ff1c
angelcepeda <jangelcepeda@protonmail.com>
2024-03-10 18:54:55 -0600

    APi pokemon generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
+import { join } from 'path'; //coming form node
+
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+    }),
+    PokemonModule
+  ],
 })
 export class AppModule {}
---
+++ src/pokemon/entities/pokemon.entity.ts
@@ -0,0 +1,3 @@
+import { PayloadTooLargeException } from "@nestjs/common"
+
+export class Pokemon {}
