3cd7e0de5b969e6b918915b70136c611bff8f525
angelcepeda <jangelcepeda@protonmail.com>
2024-03-12 21:10:59 -0600

    Schema added to model

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,3 +1,25 @@
 //representaicón de los modelos de bases de datos
+//tablas, etc.
+//Usando mongo esto es una coleccion
+import { Document } from "mongoose";
+import { Schema, SchemaFactory, Prop } from "@nestjs/mongoose";
 
-export class Pokemon {}
+//Si listamos
+
+@Schema()
+export class Pokemon extends Document{
+    
+    @Prop({
+        unique:true,
+        index:true,
+    })
+    name:string;
+
+    @Prop({
+        unique:true,
+        index:true,
+    })
+    no:number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass( Pokemon );
